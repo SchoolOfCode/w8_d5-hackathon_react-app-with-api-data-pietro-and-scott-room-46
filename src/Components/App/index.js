e489eba35ab2da71af08f63f589ab619420302bd
scottwilson <swrs@hotmail.co.uk>
2022-01-14 15:27:09 +0000

    randomizer working

--- src/Components/App/index.js
+++ src/Components/App/index.js
@@ -1,60 +1,20 @@
-import React, {useReducer, useState,useEffect} from 'react';
-import './App.css';
-import Superhero from "../SuperHeroViewer";
+import React, { useState } from "react";
+import "./App.css";
 import Card from "../Card";
 
-
-
 function App() {
+  const [id, setId] = useState(1);
 
-  const [heroName, setheroName] = useState("")
-    const [cardImage, setcardImage] = useState("")
-     const [statsState, setstatsState] = useState("")
-     const [array, setarray] = useState([])
-
-  async function fetchSuperHero(){
-
-    
-
-
-
-
-let response = await fetch("https://akabab.github.io/superhero-api/api/all.json");
-let data = await response.json();
-
-let superheroName= data[0].name;
-let superheroImage= data[0].images.md;
- 
-    for(let i=0; i<data.length; i++){
-             console.log(data[i].images.md)  
-    }
-
-setarray(data);
-setheroName(superheroName);
-setcardImage(superheroImage);
-
-
-
-
-console.log(statsState)
-
-
-
-}
-
-
- useEffect(() => {
- fetchSuperHero();
-},[])
-
+  function handleClick() {
+    let chosenId = Math.floor(Math.random({ id }) * 731);
+    setId(chosenId);
+  }
 
   return (
     <div className="App">
       <h1>Superhero API</h1>
-      <p>{heroName}</p>
-      <a href=""><img src={cardImage} /></a>
-      <Superhero />
-      <Card heroName={heroName} cardImage={cardImage} statsState={statsState} />
+      <button onClick={handleClick}>Random Superhero!</button>
+      <Card id={id} />
     </div>
   );
 }
--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,30 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-function Card({ heroName, cardImage, statsState }) {
-    return (
-      <div>
-        <p>Super hero card</p>
-        <p>{heroName}</p>
-        <img src={cardImage} />
-        <p>
-          {Object.keys(statsState)[0]} : {statsState.intelligence}
-        </p>
-        <p>
-          {Object.keys(statsState)[1]} : {statsState.strength}
-        </p>
-        <p>
-          {Object.keys(statsState)[2]} : {statsState.speed}
-        </p>
-        <p>
-          {Object.keys(statsState)[3]} : {statsState.durability}
-        </p>
-        <p>
-          {Object.keys(statsState)[4]} : {statsState.power}
-        </p>
-        <p>
-          {Object.keys(statsState)[5]} : {statsState.combat}
-        </p>
-      </div>
+function Card({ id }) {
+  const [heroName, setheroName] = useState("");
+  const [cardImage, setcardImage] = useState("");
+  const [statsState, setstatsState] = useState("");
+
+  useEffect(() => {
+    FetchSuperHero();
+  });
+  async function FetchSuperHero() {
+    const response = await fetch(
+      `https://akabab.github.io/superhero-api/api/id/${id}.json`
     );
+    const data = await response.json();
+
+    let superheroName = data.name;
+    let superheroImage = data.images.md;
+    let supeheroStats = data.powerstats;
+
+    setheroName(superheroName);
+    setcardImage(superheroImage);
+    setstatsState(supeheroStats);
   }
+  return (
+    <div>
+      <p>{heroName}</p>
+      <img src={cardImage} />
+      <p>
+        {Object.keys(statsState)[0]} : {statsState.intelligence}
+      </p>
+      <p>
+        {Object.keys(statsState)[1]} : {statsState.strength}
+      </p>
+      <p>
+        {Object.keys(statsState)[2]} : {statsState.speed}
+      </p>
+      <p>
+        {Object.keys(statsState)[3]} : {statsState.durability}
+      </p>
+      <p>
+        {Object.keys(statsState)[4]} : {statsState.power}
+      </p>
+      <p>
+        {Object.keys(statsState)[5]} : {statsState.combat}
+      </p>
+    </div>
+  );
+}
 export default Card;
